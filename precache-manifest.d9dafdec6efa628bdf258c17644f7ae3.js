7d660e2a3889896b7a65259bff62750bd61add2b
wasiak93 <kacperjacekwasiak@gmail.com>
2020-06-24 10:33:58 +0200

    Updates

---
+++ precache-manifest.d9dafdec6efa628bdf258c17644f7ae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6676a371bf529a1d542d22626c843468",
+    "url": "/gifts/index.html"
+  },
+  {
+    "revision": "560e7b00b2c9a7da17fb",
+    "url": "/gifts/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ec1d4c557f5f3053efce",
+    "url": "/gifts/static/css/main.53db49cc.chunk.css"
+  },
+  {
+    "revision": "560e7b00b2c9a7da17fb",
+    "url": "/gifts/static/js/2.64b1248b.chunk.js"
+  },
+  {
+    "revision": "41464252157070d41028fce25b69fa96",
+    "url": "/gifts/static/js/2.64b1248b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1d4c557f5f3053efce",
+    "url": "/gifts/static/js/main.8f076e97.chunk.js"
+  },
+  {
+    "revision": "218ccf66f1c17c56e150",
+    "url": "/gifts/static/js/runtime-main.b8252bed.js"
+  }
+]);
